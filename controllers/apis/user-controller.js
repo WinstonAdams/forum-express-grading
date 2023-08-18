0e05b9b58e808682cf35a47a7865f660bae17702
WinstonAdams <apollo07291@gmail.com>
2023-08-18 11:50:53 +0800

    feat: api signin add jwt token

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON() // req.user 改成拿到比對使用者密碼後回傳的 user
+      delete userData.password // 密碼不應該被回傳到前端
+
+      // 簽發 JWT token (效期為 30 天)
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const userController = require('../../controllers/apis/user-controller')
 
 router.use('/admin', admin)
+
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 關閉 session 功能 (不會做序列化和反序列化)，req.user 改成拿到比對使用者密碼後回傳的 user
 
 router.get('/restaurants', restController.getRestaurants)
 
